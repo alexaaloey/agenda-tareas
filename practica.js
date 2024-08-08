e55e4dfd4ff4f7ec1908e3f8cd0b0448c3e31856
Alexa <L21270629@tuxtla.tecnm.mx>
2024-08-07 20:59:29 -0600

    primera vez subiendo los archivos

---
+++ practica.js
@@ -0,0 +1,29 @@
+
+alert('Hola desde un alert xd') //un comentario 
+
+let nombre = "Alex";
+
+let persona = {name: 'Oscar', apellido: 'Chame', edad: '13'};
+console.log(persona);
+
+let personas =[ 
+    {name: 'Alexa', apellido: 'Chame', edad: 20},
+    {name: 'Lenin', apellido: 'Perez', edad: 21},
+    {name: 'Valeria', apellido: 'Cano', edad: 18}, persona
+]
+console.log(personas);
+console.log(personas[2]);
+
+//Funciones basicas de operaciones aritmeticas 
+function suma(params){
+    return 2+8;
+}
+
+let result = suma();
+console.log(result);
+
+function mult(a,b){
+    return a*b;
+}
+let resultado = mult(8,7);
+console.log(resultado);
